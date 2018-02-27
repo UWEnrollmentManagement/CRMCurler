fc3cc09676fbf33226066cae92fa7258081520b0
Joseph Schilz <joseph@schilz.org>
2018-02-27 10:25:58 -0800

    Initial commit.

---
+++ src/Curler.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace UWDOEM\CRM\Curler;
+
+
+class Curler
+{
+    protected $apibase;
+    protected $apiusername;
+    protected $apipassword;
+    protected $defaultOptions;
+
+    public function __construct($apibase, $apiusername, $apipassword)
+    {
+        $this->apibase = $apibase;
+        $this->apiusername = $apiusername;
+        $this->apipassword = $apipassword;
+
+        $this->defaultOptions = [
+            CURLOPT_USERPWD => "{$this->apiusername}:{$this->apipassword}",
+            CURLOPT_HTTPAUTH => CURLAUTH_NTLM,
+        ];
+    }
+
+    protected static function processHeader($header, &$responseHeaders)
+    {
+        $len = strlen($header);
+        $header = explode(':', $header, 2);
+        if (count($header) < 2) // ignore invalid headers
+            return $len;
+
+        $name = strtolower(trim($header[0]));
+        if (!array_key_exists($name, $responseHeaders))
+            $responseHeaders[$name] = [trim($header[1])];
+        else
+            $responseHeaders[$name][] = trim($header[1]);
+
+        return $len;
+
+    }
+
+    protected function get($location, $query = [], $requestHeaders = [], &$responseHeaders = [], &$responseCode = null)
+    {
+        $thisClass = static::class;
+
+        $options = [
+            CURLOPT_URL => rtrim($this->apibase, '/') . '/' . $location . '?' . http_build_query($query),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $requestHeaders,
+            CURLOPT_HEADERFUNCTION => function($curl, $header) use (&$responseHeaders, $thisClass)
+            {
+                return $thisClass::processHeader($header, $responseHeaders);
+            },
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, ($this->defaultOptions + $options));
+
+        $result = curl_exec($ch);
+        $responseCode = curl_getinfo($ch)['http_code'];
+        curl_close($ch);
+
+        return $result;
+    }
+
+    protected function post($location, $body, $requestHeaders = [], &$responseHeaders = [], &$responseCode = null)
+    {
+        $thisClass = static::class;
+
+        $options = [
+            CURLOPT_URL => rtrim($this->apibase, '/') . '/' . $location,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $body,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTPHEADER => $requestHeaders,
+            CURLOPT_HEADERFUNCTION => function($curl, $header) use (&$responseHeaders, $thisClass)
+            {
+                return $thisClass::processHeader($header, $responseHeaders);
+            },
+        ];
+
+        $ch = curl_init();
+        curl_setopt_array($ch, ($this->defaultOptions + $options));
+
+        $result = curl_exec($ch);
+        $responseCode = curl_getinfo($ch)['http_code'];
+        curl_close($ch);
+
+        return $result;
+    }
+
+}
