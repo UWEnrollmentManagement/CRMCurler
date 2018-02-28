4f3ae17aa0094ad175150cdbbe7daef9b716c414
Joseph Schilz <joseph@schilz.org>
2018-02-28 11:25:56 -0800

    Add documentation.

--- src/Curler.php
+++ src/Curler.php
@@ -6,11 +6,25 @@ namespace UWDOEM\CRM\Curler;
 
 class Curler
 {
+    /** @var string $apibase */
     protected $apibase;
+
+    /** @var string $apiusername */
     protected $apiusername;
+
+    /** @var string $apipassword */
     protected $apipassword;
+
+    /** @var array $defaultOptions */
     protected $defaultOptions;
 
+    /**
+     * Create a curler object that can issue requests to a Microsoft Dynamics CRM server.
+     *
+     * @param string $apibase     Eg: 'https://rec-test1.s.uw.edu/Seattle/api/data/v8.0/' for the admissions test CRM server
+     * @param string $apiusername Eg: 'someuser'
+     * @param string $apipassword Eg: 'somepassword'
+     */
     public function __construct($apibase, $apiusername, $apipassword)
     {
         $this->apibase = $apibase;
@@ -25,6 +39,15 @@ class Curler
         ];
     }
 
+    /**
+     * Helper function to retrieve headers from an HTTP response.
+     *
+     * See CURLOPT_HEADERFUNCTION in the (curl_setopt documention)[http://php.net/manual/en/function.curl-setopt.php].
+     *
+     * @param string $header
+     * @param array  $responseHeaders
+     * @return int
+     */
     protected static function processHeader($header, &$responseHeaders)
     {
         $len = strlen($header);
